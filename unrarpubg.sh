90dd3977b22b31194325602bb8dee7c7cfcb8950
Adityasingh-reseller <78647973+Adityasingh-reseller@users.noreply.github.com>
2022-01-05 13:55:09 +0530

    Create unrarpubg.sh

---
+++ unrarpubg.sh
@@ -0,0 +1,9 @@
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!          DOWNLOAD TOOL FILE          !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/PUBG-BGMI/SUMAN-PUBG.rar
+wget https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/OnlineHacking/SUMAN/PUBG-BGMI/SUMAN-BGMI.rar
+unrar x -Password SUMAN-PUBG.rar
+unrar x -Password SUMAN-BGMI.rar
+rm -rf SUMAN-PUBG.rar SUMAN-BGMI.rar
